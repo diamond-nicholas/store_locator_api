f7aecd599f0b15df8d5e0f637c8481a8d173d444
diamond-nicholas <diamondnicholas154@gmail.com>
2022-04-05 05:57:05 +0100

    update models for store locator

--- model/stores.js
+++ model/stores.js
@@ -1,8 +1,35 @@
 const mongoose = require('mongoose');
 
 const store = new mongoose.Schema({
-  name: String,
-  address: String,
+  storeId: {
+    type: String,
+    required: [true, 'Please add a storeID'],
+    unique: true,
+    trim: true,
+    maxlength: [10, 'storeID must be less than 10 characters'],
+  },
+
+  address: {
+    type: String,
+    required: [true, 'Please enter an address'],
+  },
+
+  location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+    },
+    coordinates: {
+      type: [Array],
+      required: '2dsphere',
+    },
+    formattedAddress: String,
+  },
+
+  createdAt: {
+    type: Date,
+    Default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('store', store);
