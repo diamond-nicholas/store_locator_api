3d5f0ac25e7aaa18842f8cf50797f75e56b44d07
diamond-nicholas <diamondnicholas154@gmail.com>
2022-04-05 06:01:39 +0100

    add logic to get all stores

--- controllers/store.js
+++ controllers/store.js
@@ -2,7 +2,16 @@ const Store = require('../model/stores');
 
 const addStore = async (req, res) => {
   try {
-  } catch (error) {}
+    const store = await Store.find({});
+    return res.status(200).json({
+      success: true,
+      count: store.length,
+      data: store,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'server error' });
+  }
 };
 
 module.exports = {
